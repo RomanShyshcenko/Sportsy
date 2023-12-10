c5012eba29504d94ded57566e379707a95b62616
Roman Shyshchenko <shyshchenkoroman@gmail.com>
2023-12-10 13:11:33 +0200

    create core/settings and base settings

---
+++ app/core/settings/base.py
@@ -0,0 +1,261 @@
+import os
+from os.path import join
+from pathlib import Path
+from dotenv import load_dotenv
+
+import datetime
+from distutils.util import strtobool
+from configurations import Configuration
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
+
+
+class Base(Configuration):
+
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+
+
+        # Third party apps
+        'rest_framework',
+        'rest_auth',                 # django-rest-auth (API endpoints for User operations)
+        'django_filters',            # for filtering rest endpoints
+        'django.contrib.sites',      # used by django-allauth
+        'corsheaders',
+        'allauth',                   # django-allauth
+        'allauth.account',
+        'rest_auth.registration',    # django-rest-auth using django-allauth
+        'allauth.socialaccount',
+        'allauth.socialaccount.providers.facebook',
+        'allauth.socialaccount.providers.google',
+        'allauth.socialaccount.providers.twitter',
+
+        # Your apps
+        # 'backend.users',
+
+    )
+
+    # https://docs.djangoproject.com/en/2.0/topics/http/middleware/
+    MIDDLEWARE = (
+        'django.middleware.security.SecurityMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'corsheaders.middleware.CorsMiddleware',
+        'allauth.account.middleware.AccountMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
+
+    ALLOWED_HOSTS = ["*"]
+    ROOT_URLCONF = 'core.urls'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    WSGI_APPLICATION = 'core.wsgi.application'
+
+    ADMINS = (
+        ('Author', 'example@example.com'),
+    )
+
+    # Postgres
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.getenv("NAME", 'postgres'),
+            'USER': os.getenv("DB_USER", 'postgres'),
+            'PASSWORD': os.getenv("PASSWORD", 'postgres'),
+            'HOST': os.getenv("HOST", 'localhost'),
+            'PORT': os.getenv("PORT", '5432'),
+        }
+    }
+
+    # General
+    APPEND_SLASH = False
+    TIME_ZONE = 'UTC'
+    LANGUAGE_CODE = 'en-us'
+    # If you set this to False, Django will make some optimizations so as not
+    # to load the internationalization machinery.
+    USE_I18N = False
+    USE_L10N = True
+    USE_TZ = True
+    LOGIN_REDIRECT_URL = '/'
+
+    STATIC_URL = 'static/'
+
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [BASE_DIR / ''],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                    'django.template.context_processors.request',
+                ],
+            },
+        },
+    ]
+
+    # DEBUG was set to False as a default for safety
+    # https://docs.djangoproject.com/en/dev/ref/settings/#debug
+    DEBUG = strtobool(str(os.getenv('DJANGO_DEBUG', True)))
+
+    # Password Validation
+    # https://docs.djangoproject.com/en/2.0/topics/auth/passwords/#module-django.contrib.auth.password_validation
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
+
+    # Logging
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'django.server': {
+                '()': 'django.utils.log.ServerFormatter',
+                'format': '[%(server_time)s] %(message)s',
+            },
+            'verbose': {
+                'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+            },
+            'simple': {
+                'format': '%(levelname)s %(message)s'
+            },
+        },
+        'filters': {
+            'require_debug_true': {
+                '()': 'django.utils.log.RequireDebugTrue',
+            },
+        },
+        'handlers': {
+            'django.server': {
+                'level': 'INFO',
+                'class': 'logging.StreamHandler',
+                'formatter': 'django.server',
+            },
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'simple'
+            },
+            'mail_admins': {
+                'level': 'ERROR',
+                'class': 'django.utils.log.AdminEmailHandler'
+            }
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['console'],
+                'propagate': True,
+            },
+            'django.server': {
+                'handlers': ['django.server'],
+                'level': 'INFO',
+                'propagate': False,
+            },
+            'django.request': {
+                'handlers': ['mail_admins', 'console'],
+                'level': 'ERROR',
+                'propagate': False,
+            },
+            'django.db.backends': {
+                'handlers': ['console'],
+                'level': 'INFO'
+            },
+        }
+    }
+
+    # Custom user app
+    # AUTH_USER_MODEL = 'users.User'
+
+    # Django Rest Framework
+    REST_FRAMEWORK = {
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': int(os.getenv('DJANGO_PAGINATION_LIMIT', 10)),
+        'DATETIME_FORMAT': '%Y-%m-%dT%H:%M:%S%z',
+        'DEFAULT_RENDERER_CLASSES': (
+            'rest_framework.renderers.JSONRenderer',
+            'rest_framework.renderers.BrowsableAPIRenderer',
+        ),
+        'DEFAULT_PERMISSION_CLASSES': [
+            'rest_framework.permissions.IsAuthenticated',
+        ],
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+            # Make JWT Auth the default authentication mechanism for Django
+            'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        )
+    }
+
+    # django-rest-auth + django-allauth registration (required for django.contrib.sites)
+    SITE_ID = 1
+
+    # Enables django-rest-auth to use JWT tokens instead of regular tokens.
+    REST_USE_JWT = True
+
+    # Configure the JWTs to expire after 1 hour, and allow users to refresh near-expiration tokens
+    JWT_AUTH = {
+        'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=1),
+        'JWT_ALLOW_REFRESH': True,
+        'JWT_AUTH_HEADER_PREFIX': 'Bearer',
+    }
+
+    REST_AUTH_SERIALIZERS = {
+        'USER_DETAILS_SERIALIZER': 'backend.users.serializers.UserSerializer',
+        'PASSWORD_RESET_SERIALIZER': 'backend.users.serializers.CustomPasswordResetSerializer'
+    }
+
+    AUTHENTICATION_BACKENDS = (
+        # allauth specific authentication methods, such as login by e-mail
+        'allauth.account.auth_backends.AuthenticationBackend',
+    )
+
+    REST_AUTH_REGISTER_SERIALIZERS = {
+        'REGISTER_SERIALIZER': 'backend.users.serializers.RegisterSerializer',
+    }
+
+    # Remove username functionality. Email is identifier (django-allauth)
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = 'email'  # ( = "username" | "email" | "username_email )
+    ACCOUNT_UNIQUE_EMAIL = True
+    # ACCOUNT_EMAIL_VERIFICATION = 'optional'
+
+    # Email Backend
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = os.getenv('DJANGO_EMAIL_HOST', 'localhost')
+
+    EMAIL_PORT = os.getenv('DJANGO_EMAIL_PORT', 1025)
+    EMAIL_HOST_USER = os.getenv('DJANGO_EMAIL_HOST_USER', 'example')
+    EMAIL_HOST_PASSWORD = os.getenv('DJANGO_EMAIL_HOST_PASSWORD', 'example')
+
+    # Automatic mails
+    DEFAULT_FROM_EMAIL = os.getenv('DJANGO_DEFAULT_FROM_EMAIL', 'hi@example.com')
+    ACCOUNT_EMAIL_SUBJECT_PREFIX = os.getenv('DJANGO_ACCOUNT_EMAIL_SUBJECT_PREFIX', '[Example]')
+
+    # Account verification email
+    ACCOUNT_ADAPTER = 'backend.users.adapter.DefaultAccountAdapterCustom'
+    URL_FRONT = os.getenv('DJANGO_URL_FRONT', 'localhost:8080')
+    # ACCOUNT_CONFIRM_EMAIL_ON_GET = True
+
+    OLD_PASSWORD_FIELD_ENABLED = True
+    LOGOUT_ON_PASSWORD_CHANGE = True
