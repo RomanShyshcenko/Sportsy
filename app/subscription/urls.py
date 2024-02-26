47911d41f4b399e8eb072feeccb4787b8de09040
Roman Shyshchenko <shyshchenkoroman@gmail.com>
2024-02-26 13:10:23 +0200

    Add app_name=subscription

--- app/subscription/urls.py
+++ app/subscription/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from subscription import views
 
+app_name = 'subscription'
+
 urlpatterns = [
     path('subscriptions/create/', views.SubscriptionCreateAPIView.as_view(), name='create-subscription'),
     path('subscriptions/', views.SubscriptionRetrieveUpdateAPIView.as_view(), name='retrieve-subscription')
